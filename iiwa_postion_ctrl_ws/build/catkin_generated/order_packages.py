f7f2ff55b391905cd5afd50fa99d876b07d0e187
niederha <loic.niederhauser@epfl.ch>
2022-06-14 17:46:37 +0200

    Removed wrongly tracked files

--- iiwa_postion_ctrl_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ros/iiwa_position_ctrl_interface/iiwa_postion_ctrl_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ros/ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/ros/ros_ws/devel;/opt/ros/noetic' != '' else []
--- iiwa_postion_ctrl_ws/build/iiwa_position_ctrl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/iiwa_position_ctrl_interface/iiwa_postion_ctrl_ws/src/iiwa_position_ctrl
-# Build directory: /home/ros/iiwa_position_ctrl_interface/iiwa_postion_ctrl_ws/build/iiwa_position_ctrl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
